277adc7bc7731dcd7b880ece19aa0682c7554b3d
hisoka <hisoka>
2025-09-30 17:48:54 +0200

    Set up sanity and generate types for cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -6,6 +9,21 @@ export default async function Home({
   searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = await client.fetch(STARTUPS_QUERY)
+  // const posts = [
+  //   {
+  //     _createdAt: new Date(),
+  //     views: 55,
+  //     author: { _id: 1, name:"his" },
+  //     _id: 1,
+  //     description: "This is description",
+  //     image:
+  //       "https://media.istockphoto.com/id/2149530993/photo/digital-human-head-concept-for-ai-metaverse-and-facial-recognition-technology.jpg?s=612x612&w=0&k=20&c=IduORJUs1c1s0m2SXQANsK8IUhtlz8QApsLxNYOYrXQ=",
+  //     category: "Robots",
+  //     title: "We Robots",
+  //   },
+  // ];
+  console.log(JSON.stringify(posts, null, 2))
   return (
     <>
       <section className="pink_container">
@@ -19,6 +37,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section-container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post:StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
